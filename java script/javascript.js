04698223f0be263022cd1c666e001d897ba47df9
ElisFliess <59352894+Matito-7@users.noreply.github.com>
2024-11-29 10:56:53 -0300

    atualização

--- java script/javascript.js
+++ java script/javascript.js
@@ -1,13 +1,13 @@
 function selectOption(opcao) {
     const chatBox = document.querySelector('.chat-box');
 
-    // Cria um novo elemento para a mensagem do usuário
+    
     const userMessage = document.createElement('div');
     userMessage.classList.add('user-message');
     userMessage.textContent = `Você: ${opcao}`;
     chatBox.appendChild(userMessage);
 
-    // Simula uma resposta do bot com base na opção escolhida
+  
     let botMessage;
     if (opcao === 'Dor ou desconforto') {
         botMessage = "Enfermeiro: Estou a caminho!";
@@ -29,18 +29,18 @@ function selectOption(opcao) {
     botMessageElement.textContent  = botMessage;
        chatBox.appendChild(botMessageElement);
    
-       // Rola a conversa para baixo
+       
        chatBox.scrollTop = chatBox.scrollHeight;
    }
 
    const balloons = document.querySelectorAll('.balloon');
   const chatBox = document.getElementById('chat-box');
 
-// Objeto com perguntas e respostas pré-definidas
+
 const respostas = {
   'Pergunta 1': 'Esta é a resposta para a pergunta 1.',
   'Pergunta 2': 'A resposta para a pergunta 2 é esta.',
-  // Adicione mais perguntas e respostas aqui
+
 };
 
 balloons.forEach(balloon => {
